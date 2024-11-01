a692d29492ee5a281aba6edb4f4c0676978b9126
mvrzan <mvrzan@icloud.com>
2024-11-01 14:53:22 -0400

    update chat widget component

--- src/components/ChatWidget/ChatWidget.tsx
+++ src/components/ChatWidget/ChatWidget.tsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
 import { HStack } from "@chakra-ui/react";
+
+import ChatCanvas from "./ChatCanvas";
+
 import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const ChatWidget = () => {
+  const [chatIsOpen, setChatIsOpen] = useState(false);
+
+  const openChatHandler = () => {
+    setChatIsOpen(!chatIsOpen);
+  };
+
   return (
-    <HStack>
-      <Button colorPalette="purple" position="fixed" bottom="20px" right="20px" zIndex="1000" borderRadius="30px">
-        <IoChatbubbleEllipsesSharp /> Start Chat
-      </Button>
-    </HStack>
+    <>
+      <HStack>
+        <Button
+          colorPalette="purple"
+          position="fixed"
+          bottom="20px"
+          right="20px"
+          zIndex="1000"
+          borderRadius="30px"
+          onClick={openChatHandler}
+        >
+          {chatIsOpen ? (
+            <>
+              <IoMdCloseCircle /> Close Chat
+            </>
+          ) : (
+            <>
+              <IoChatbubbleEllipsesSharp /> Start Chat
+            </>
+          )}
+        </Button>
+      </HStack>
+      {chatIsOpen && <ChatCanvas />}
+    </>
   );
 };
 
