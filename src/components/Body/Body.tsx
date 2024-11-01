873d3956dc34f113aa3bb130906e79726c654b40
mvrzan <mvrzan@icloud.com>
2024-11-01 10:17:11 -0400

    add product Recommendations component

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,35 +1,40 @@
 import { Flex, HStack, Text, VStack } from "@chakra-ui/react";
 
+import Recommendations from "./Recommendations";
+
 const Body = () => {
   return (
-    <Flex align="start" justify="center" paddingX="100px" bg="white" height="50vh" marginTop="20px">
-      <VStack>
-        <Text
-          fontSize="5xl"
-          fontWeight="extrabold"
-          bgClip="text"
-          css={{ background: "linear-gradient(to right, #66B2FF, #A366FF, #CC99FF)" }}
-        >
-          Einstein Personalization
-        </Text>
-        <Text fontSize="2xl" fontWeight="normal" fontStyle="italic">
-          Discovery how you can user real-time Einstein Personalization
-        </Text>
-        <HStack>
-          <Text fontSize="2xl" fontWeight="normal" fontStyle="italic">
-            combined with
-          </Text>
+    <>
+      <Flex align="start" justify="center" paddingX="100px" bg="white" height="25vh" marginTop="20px">
+        <VStack>
           <Text
-            fontSize="2xl"
+            fontSize="5xl"
             fontWeight="extrabold"
             bgClip="text"
             css={{ background: "linear-gradient(to right, #66B2FF, #A366FF, #CC99FF)" }}
           >
-            Agentforce!
+            Einstein Personalization
           </Text>
-        </HStack>
-      </VStack>
-    </Flex>
+          <Text fontSize="2xl" fontWeight="normal" fontStyle="italic">
+            Discovery how you can user real-time Einstein Personalization
+          </Text>
+          <HStack>
+            <Text fontSize="2xl" fontWeight="normal" fontStyle="italic">
+              combined with
+            </Text>
+            <Text
+              fontSize="2xl"
+              fontWeight="extrabold"
+              bgClip="text"
+              css={{ background: "linear-gradient(to right, #66B2FF, #A366FF, #CC99FF)" }}
+            >
+              Agentforce!
+            </Text>
+          </HStack>
+        </VStack>
+      </Flex>
+      <Recommendations />
+    </>
   );
 };
 
