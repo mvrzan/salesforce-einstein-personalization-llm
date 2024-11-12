2b98e11ccda6c7bcf127180483f7e0c175466352
mvrzan <mvrzan@icloud.com>
2024-11-12 10:28:33 -0500

    create a login modal component

---
+++ src/components/Header/LoginModal.tsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
+import { Input } from "@chakra-ui/react";
+
+import { writeToLocalStorage } from "../../utils/localStorageUtil";
+
+interface LoginModalProps {
+  isLoginModalOpen: boolean;
+  setIsLoginModalOpen: (isOpen: boolean) => void;
+}
+
+const LoginModal = ({ isLoginModalOpen, setIsLoginModalOpen }: LoginModalProps) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    if (firstName && lastName && email) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [firstName, lastName, email]);
+
+  const loginHandler = () => {
+    writeToLocalStorage("isAuthenticated", "true");
+    writeToLocalStorage("user", {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+    });
+
+    setIsLoginModalOpen(false);
+  };
+
+  return (
+    <DialogRoot
+      open={isLoginModalOpen}
+      placement="center"
+      onOpenChange={() => {
+        setIsLoginModalOpen(!isLoginModalOpen);
+      }}
+    >
+      <DialogTrigger />
+      <DialogContent bg="white">
+        <DialogHeader>
+          <DialogTitle color="black">Sign In!</DialogTitle>
+        </DialogHeader>
+        <DialogBody>
+          <Field color="black" label="First name">
+            <Input
+              variant="outline"
+              placeholder="First name..."
+              _placeholder={{ color: "gray" }}
+              borderColor="#9333ea"
+              focusRingColor="purple"
+              onChange={(event) => {
+                setFirstName(event.target.value);
+              }}
+            />
+          </Field>
+          <Field color="black" label="Last name">
+            <Input
+              variant="outline"
+              placeholder="Last name..."
+              _placeholder={{ color: "gray" }}
+              borderColor="#9333ea"
+              focusRingColor="purple"
+              onChange={(event) => {
+                setLastName(event.target.value);
+              }}
+            />
+          </Field>
+          <Field color="black" label="Email address">
+            <Input
+              variant="outline"
+              placeholder="email@example.com"
+              _placeholder={{ color: "gray" }}
+              borderColor="#9333ea"
+              focusRingColor="purple"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+          </Field>
+          <Field color="black" label="Phone number">
+            <Input
+              variant="outline"
+              placeholder="+123456789"
+              _placeholder={{ color: "gray" }}
+              borderColor="#9333ea"
+              focusRingColor="purple"
+              onChange={(event) => {
+                setPhoneNumber(event.target.value);
+              }}
+            />
+          </Field>
+        </DialogBody>
+        <DialogFooter>
+          <DialogActionTrigger asChild>
+            <Button colorPalette="red">Cancel</Button>
+          </DialogActionTrigger>
+          <Button colorPalette="purple" onClick={loginHandler} disabled={isButtonDisabled}>
+            Save changes
+          </Button>
+        </DialogFooter>
+        <DialogCloseTrigger />
+      </DialogContent>
+    </DialogRoot>
+  );
+};
+
+export default LoginModal;
