8a56d00a5380b74b5810fb1a39e4ab12139adce0
mvrzan <mvrzan@icloud.com>
2024-11-13 10:09:25 -0500

    add script configuration from local storage in App component on component load

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { useEffect } from "react";
+import useScript from "./hooks/useScript";
+
+import { readFromLocalStorage } from "./utils/localStorageUtil";
+
 import { Theme } from "@chakra-ui/react";
 
 import Header from "./components/Header/Header";
@@ -9,6 +14,21 @@ import ChatWidget from "./components/ChatWidget/ChatWidget";
 import AgentChatWidget from "./components/ChatWidget/AgentChatWidget";
 
 const App = () => {
+  const configureScriptUrl = useScript();
+  useEffect(() => {
+    const existingScript = document.querySelector('script[src*="c360a.min.js"]');
+    if (existingScript) {
+      return;
+    }
+
+    const scriptUrl = readFromLocalStorage("scriptUrl");
+
+    if (scriptUrl) {
+      configureScriptUrl(scriptUrl);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Theme appearance="light">
       <Header />
