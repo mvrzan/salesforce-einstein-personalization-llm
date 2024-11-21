780526411bf708e3b18c06d20412b0bfcc9f28ca
mvrzan <mvrzan@icloud.com>
2024-11-21 12:05:12 -0500

    add viewProduct function to SalesforceInteractions hook for product detail tracking

--- src/hooks/useSalesforceInteractions.ts
+++ src/hooks/useSalesforceInteractions.ts
@@ -8,6 +8,7 @@ declare const window: Window &
 
 interface SalesforceInteractions {
   startChat: () => void;
+  viewProduct: (id: number, productName: string, productDescription: string) => void;
   userLoggedInHook: (firstName: string, lastName: string, email: string, phoneNumber: string) => void;
   userLoggedOutHook: (firstName: string, lastName: string, email: string, phoneNumber: string) => void;
 }
@@ -81,6 +82,62 @@ const useSalesforceInteractions = (): SalesforceInteractions => {
     });
   };
 
+  const viewProductDetailsFunction = (id: number, productName: string, productDescription: string) => {
+    if (window.SalesforceInteractions === undefined) {
+      return;
+    }
+
+    if (userLoggedIn) {
+      const user = JSON.parse(readFromLocalStorage("user") as string);
+
+      const attributes = {
+        eventType: "identity",
+        firstName: user.firstName,
+        lastName: user.lastName,
+        isAnonymous: "1",
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+      };
+
+      // Send to Salesforce Data Cloud that a known user viewed a product
+      window.SalesforceInteractions.sendEvent({
+        interaction: {
+          name: window.SalesforceInteractions.CatalogObjectInteractionName.ViewCatalogObject,
+          catalogObject: {
+            type: "Product",
+            id: id.toString(),
+            attributes: {
+              productName,
+              productDescription,
+            },
+          },
+        },
+        user: {
+          attributes,
+        },
+      });
+
+      return;
+    }
+
+    // Send to Salesforce Data Cloud that an unknown user viewed the details of a product
+    window.SalesforceInteractions.sendEvent({
+      interaction: {
+        name: window.SalesforceInteractions.CatalogObjectInteractionName.ViewCatalogObject,
+        catalogObject: {
+          type: "Product",
+          id: id.toString(),
+          attributes: {
+            productName,
+            productDescription,
+          },
+        },
+      },
+    });
+
+    return;
+  };
+
   const userLoggedInFunction = (firstName: string, lastName: string, email: string, phoneNumber: string) => {
     if (window.SalesforceInteractions === undefined) {
       return;
@@ -127,6 +184,7 @@ const useSalesforceInteractions = (): SalesforceInteractions => {
 
   return {
     startChat: startChatFunction,
+    viewProduct: viewProductDetailsFunction,
     userLoggedInHook: userLoggedInFunction,
     userLoggedOutHook: userLoggedOutFunction,
   };
