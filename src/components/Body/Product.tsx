5adff0fdd3664e0a960cb6763f910339f991e656
mvrzan <mvrzan@icloud.com>
2024-12-02 17:46:20 -0500

    refactor Product component layout for improved structure and styling

--- src/components/Body/Product.tsx
+++ src/components/Body/Product.tsx
@@ -8,26 +8,28 @@ interface ProductProps {
 
 const Product = ({ image, productName, description }: ProductProps) => {
   return (
-    <Box
-      as="button"
-      border="solid"
-      borderRadius="8px"
-      borderWidth="1px"
-      borderColor="black"
-      height="400px"
-      width="300px"
-      _hover={{ boxShadow: "0 0 10px 2px purple", borderColor: "purple" }}
-    >
-      <VStack>
-        <Image src={image} alt="camera product image" height="100%" width="100%" borderRadius="8px" />
-        <Text fontSize="20px" fontWeight="extrabold">
-          {productName}
-        </Text>
-        <Text fontSize="14px" fontWeight="medium" padding="15px" textAlign="center">
-          {description}
-        </Text>
-      </VStack>
-    </Box>
+    <VStack>
+      <Box
+        as="button"
+        border="solid"
+        borderRadius="8px"
+        borderWidth="1px"
+        borderColor="black"
+        height="400px"
+        width="300px"
+        _hover={{ boxShadow: "0 0 10px 2px purple", borderColor: "purple" }}
+      >
+        <VStack height="100%" justifyContent="flex-start">
+          <Image src={image} alt="camera product image" height="200px" width="100%" borderRadius="8px" />
+          <Text fontSize="20px" fontWeight="extrabold">
+            {productName}
+          </Text>
+          <Text fontSize="14px" fontWeight="medium" padding="15px" textAlign="center">
+            {description}
+          </Text>
+        </VStack>
+      </Box>
+    </VStack>
   );
 };
 
