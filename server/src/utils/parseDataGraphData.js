d15d3175df2e310ded1db01b4fbd0842955fb64c
mvrzan <mvrzan@icloud.com>
2024-12-13 18:19:35 -0500

    add parseDataGraph utility function to format chat messages from data graph

---
+++ server/src/utils/parseDataGraphData.js
@@ -0,0 +1,30 @@
+export const parseDataGraph = (dataGraph) => {
+  const decodeJson = (str) =>
+    str
+      .replace(/&quot;/g, '"')
+      .replace(/&amp;/g, "&")
+      .replace(/&lt;/g, "<")
+      .replace(/&gt;/g, ">")
+      .replace(/&#39;/g, "'");
+
+  const decodedJson = decodeJson(dataGraph.data[0].json_blob__c);
+  const parsedJson = JSON.parse(decodedJson);
+  const individual = parsedJson.UnifiedLinkssotIndividualMcp__dlm[0].ssot__Individual__dlm;
+  const chatMessages = individual[0].chatActivities__dlm;
+  const deviceId = parsedJson.UnifiedLinkssotIndividualMcp__dlm[0].SourceRecordId__c;
+
+  const formattedChatMessages = chatMessages?.map((chatMessage) => {
+    return { chatMessage: chatMessage.chatActivities_chatMessage__c, dateTime: chatMessage.dateTime__c };
+  });
+
+  const formattedDataGraphObject = {
+    inputs: [
+      {
+        deviceId: deviceId,
+        chatBody: JSON.stringify(formattedChatMessages),
+      },
+    ],
+  };
+
+  return formattedDataGraphObject;
+};
