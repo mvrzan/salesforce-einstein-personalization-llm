7989687a5efaa621e00f9802215652f7957605d3
mvrzan <mvrzan@icloud.com>
2024-12-13 18:19:29 -0500

    add epRecommendedProducts state and updateRecommendedProducts action to useBearStore

--- client/src/hooks/useBearStore.ts
+++ client/src/hooks/useBearStore.ts
@@ -1,14 +1,21 @@
 import { create } from "zustand";
+import { ProductType } from "@/utils/types";
 
 type BearStore = {
   bannerImage: string;
+  epRecommendedProducts: ProductType[];
+  updateRecommendedProducts: (products: ProductType[]) => void;
   setBannerImage: (image: string) => void;
   resetBannerImage: () => void;
 };
+
 const useBearStore = create<BearStore>((set) => ({
   bannerImage: "",
   setBannerImage: (image: string) => set({ bannerImage: image }),
   resetBannerImage: () => set({ bannerImage: "" }),
+
+  epRecommendedProducts: [],
+  updateRecommendedProducts: (products: ProductType[]) => set({ epRecommendedProducts: products }),
 }));
 
 export default useBearStore;
