df55e18733e98a29490f4ca323070aa1ab1c97b2
mvrzan <mvrzan@icloud.com>
2024-12-13 18:19:41 -0500

    add server setup with Express, CORS, and body-parser middleware

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import router from "./src/routes/route-collection.js";
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
