77a99d6a90c25be1654eb9662b881afdfdc514c2
mvrzan <mvrzan@icloud.com>
2024-12-13 18:19:10 -0500

    add global type declaration for SalesforceInteractions on Window object

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -3,3 +3,11 @@ export interface ProductType {
   ImageURL__c: string;
   ssot__Name__c: string;
 }
+
+declare global {
+  export interface Window {
+    SalesforceInteractions: {
+      getAnonymousId: () => string;
+    };
+  }
+}
