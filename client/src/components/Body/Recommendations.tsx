892d71c1a61e5c2c15d2809ed7d2bf88c9dc368c
mvrzan <mvrzan@icloud.com>
2024-12-17 15:19:27 -0500

    add border animation and category selection to Recommendations component

--- client/src/components/Body/Recommendations.tsx
+++ client/src/components/Body/Recommendations.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useBearStore from "@/hooks/useBearStore";
+import { keyframes } from "@emotion/react";
 
 import { HStack, Text, Box, Image, VStack, Spacer, Center } from "@chakra-ui/react";
 import { Toaster, toaster } from "@/components/ui/toaster";
@@ -13,12 +14,49 @@ import headphoneImage from "../../assets/headphones.jpg";
 import droneImage from "../../assets/drone.jpg";
 import smartwatchImage from "../../assets/smart-watch.jpg";
 
+const borderAnimation = keyframes`
+  0% {
+    border-width: 1px;
+    border-color: transparent;
+    box-shadow: none;
+  }
+  25% {
+    border-width: 4px;
+    border-color: purple;
+    box-shadow: 0 0 10px 2px purple;
+  }
+  50% {
+    border-width: 1px;
+    border-color: transparent;
+    box-shadow: none;
+  }
+  75% {
+    border-width: 4px;
+    border-color: purple;
+    box-shadow: 0 0 10px 2px purple;
+  }
+  100% {
+    border-width: 1px;
+    border-color: transparent;
+    box-shadow: none;
+  }
+`;
+
 const Recommendations = () => {
   const { viewProduct, personalizationProductRecommendations } = useSalesforceInteractions();
   const [recommendedProducts, setRecommendedProducts] = useState<ProductType[]>([]);
   const [resetRecommendationsVisible, setResetRecommendationsVisible] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
   const epRecommendedProducts = useBearStore((state) => state.epRecommendedProducts);
   const updateRecommendedProducts = useBearStore((state) => state.updateRecommendedProducts);
+
+  useEffect(() => {
+    if (epRecommendedProducts.length === 0) return;
+
+    const productCategory = epRecommendedProducts[0]?.ssot__PrimaryProductCategory__c;
+    setSelectedCategory(productCategory);
+    setResetRecommendationsVisible(true);
+  }, [epRecommendedProducts]);
 
   const productClickedHandler = async (id: number, productName: string, productDescription: string) => {
     viewProduct(id, productName, productDescription);
@@ -26,6 +64,7 @@ const Recommendations = () => {
     const products = await personalizationProductRecommendations(["recsEP1"], id.toString());
     setRecommendedProducts(products);
     setResetRecommendationsVisible(true);
+    setSelectedCategory(productName);
 
     return toaster.create({
       title: `${productName} info sent to Data Cloud!`,
@@ -37,6 +76,7 @@ const Recommendations = () => {
     setResetRecommendationsVisible(false);
     setRecommendedProducts([]);
     updateRecommendedProducts([]);
+    setSelectedCategory("");
   };
 
   return (
@@ -44,7 +84,7 @@ const Recommendations = () => {
       <Toaster />
       <HStack align="start" paddingX="100px" paddingBottom="40px">
         <Text fontSize="3xl" fontWeight="extrabold" color="#9333ea" textAlign="start">
-          Product recommendations
+          Product Categories
         </Text>
         <Spacer />
         {resetRecommendationsVisible && (
@@ -66,6 +106,7 @@ const Recommendations = () => {
             onClick={() => {
               productClickedHandler(1001, "Camera", "A really good camera");
             }}
+            animation={selectedCategory === "Camera" ? `${borderAnimation} 2s infinite` : "none"}
           >
             <VStack>
               <Image src={cameraImage} alt="camera product image" height="100%" width="100%" borderRadius="8px" />
@@ -88,13 +129,15 @@ const Recommendations = () => {
             width="300px"
             _hover={{ boxShadow: "0 0 10px 2px purple", borderColor: "purple" }}
             onClick={() => productClickedHandler(1002, "Headphones", "Really good headphones")}
+            animation={selectedCategory === "Headphones" ? `${borderAnimation} 2s infinite` : "none"}
+            paddingBottom="30px"
           >
             <VStack>
               <Image src={headphoneImage} alt="headphone product image" height="100%" width="100%" borderRadius="8px" />
               <Text fontSize="20px" fontWeight="extrabold">
                 Headphones
               </Text>
-              <Text fontSize="14px" fontWeight="medium" padding="15px" textAlign="center">
+              <Text fontSize="14px" fontWeight="medium" textAlign="center">
                 Experience unparalleled sound quality with our premium headphones. Designed for comfort and equipped
                 with noise-canceling technology, they deliver an immersive audio experience for music lovers and
                 professionals alike.
@@ -104,19 +147,21 @@ const Recommendations = () => {
           <Box
             as="button"
             border="solid"
-            borderRadius="8px"
+            borderRadius="11px"
             borderWidth="1px"
             height="400px"
             width="300px"
+            paddingBottom="50px"
             _hover={{ boxShadow: "0 0 10px 2px purple", borderColor: "purple" }}
             onClick={() => productClickedHandler(1003, "Drone", "Top of the line drone")}
+            animation={selectedCategory === "Drone" ? `${borderAnimation} 2s infinite` : "none"}
           >
             <VStack>
               <Image src={droneImage} alt="drone product image" height="100%" width="100%" borderRadius="8px" />
               <Text fontSize="20px" fontWeight="extrabold">
                 Drone
               </Text>
-              <Text fontSize="14px" fontWeight="medium" padding="15px" textAlign="center">
+              <Text fontSize="14px" fontWeight="medium" textAlign="center">
                 Take your photography and videography to new heights with our cutting-edge drone. With its
                 high-definition camera and intuitive controls, capturing breathtaking aerial footage has never been
                 easier.
@@ -132,6 +177,7 @@ const Recommendations = () => {
             width="300px"
             _hover={{ boxShadow: "0 0 10px 2px purple", borderColor: "purple" }}
             onClick={() => productClickedHandler(1004, "Smartwatch", "Best smartwatch out there")}
+            animation={selectedCategory === "Smartwatch" ? `${borderAnimation} 2s infinite` : "none"}
           >
             <VStack>
               <Image
@@ -153,7 +199,7 @@ const Recommendations = () => {
         </HStack>
       </VStack>
       <Center>
-        <HStack gap="10">
+        <HStack gap="10" marginBottom="40px">
           {recommendedProducts?.map((product) => {
             return (
               <Product
