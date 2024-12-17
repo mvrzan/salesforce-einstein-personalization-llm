058db45a21de17830db2f590727e09b9d5d55468
mvrzan <mvrzan@icloud.com>
2024-12-17 15:24:21 -0500

    enhance Product component with dynamic theme support and improved styling

--- client/src/components/Body/Product.tsx
+++ client/src/components/Body/Product.tsx
@@ -1,4 +1,5 @@
 import { Text, Box, Image, VStack } from "@chakra-ui/react";
+import useBearStore from "@/hooks/useBearStore";
 
 interface ProductProps {
   image: string;
@@ -7,23 +8,36 @@ interface ProductProps {
 }
 
 const Product = ({ image, productName, description }: ProductProps) => {
+  const theme = useBearStore((state) => state.theme);
+
+  const themeSelector = () => {
+    const themeColors: { [key: string]: string } = {
+      dark: "purple",
+      teal: "teal",
+      gray: "gray",
+      green: "green",
+    };
+    return themeColors[theme] || "defaultColor";
+  };
+
   return (
     <VStack>
       <Box
         as="button"
         border="solid"
-        borderRadius="8px"
+        borderRadius="10px"
         borderWidth="1px"
+        borderColor={themeSelector()}
         height="400px"
         width="300px"
-        _hover={{ boxShadow: "0 0 10px 2px purple", borderColor: "purple" }}
+        _hover={{ boxShadow: `0 0 10px 2px ${themeSelector()}`, borderColor: `${themeSelector()}` }}
       >
         <VStack height="100%" justifyContent="flex-start">
           <Image src={image} alt="camera product image" height="200px" width="100%" borderRadius="8px" />
-          <Text fontSize="20px" fontWeight="extrabold">
+          <Text fontSize="24px" fontWeight="extrabold" color={themeSelector()}>
             {productName}
           </Text>
-          <Text fontSize="14px" fontWeight="medium" padding="15px" textAlign="center">
+          <Text fontSize="14px" fontWeight="medium" padding="15px" textAlign="center" color={themeSelector()}>
             {description}
           </Text>
         </VStack>
