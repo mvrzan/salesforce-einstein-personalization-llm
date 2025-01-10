bd1e6b0d24bdb4718ed70837a3ad85ced6ab2ad0
mvrzan <mvrzan@icloud.com>
2025-01-10 12:47:04 -0500

    update log message for notification server startup

--- server/index.js
+++ server/index.js
@@ -17,5 +17,5 @@ app.use(cors(corsOptions));
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Notification server listening on port: ${port}`);
 });
