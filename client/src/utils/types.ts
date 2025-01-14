c8d0250687fa2d52ba00db746970855d84b9f9a7
mvrzan <mvrzan@icloud.com>
2025-01-14 09:20:50 -0500

    feat(types): add new types for event handling and personalization

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -12,6 +12,46 @@ declare global {
       ConsentPurpose: { Tracking: string };
       ConsentStatus: { OptIn: string };
       getAnonymousId: () => string;
+      CatalogObjectInteractionName: {
+        ViewCatalogObject: string;
+      };
+      sendEvent: (event: {
+        interaction: {
+          name: string;
+          chatMessage?: string;
+          eventType?: string;
+          category?: string;
+          catalogObject?: {
+            type: string;
+            id: string;
+            attributes: {
+              productDescription?: string;
+              productName?: string;
+            };
+          };
+          firstName?: string;
+          lastName?: string;
+          email?: string;
+          phoneNumber?: string;
+          isAnonymous?: string;
+        };
+        user?: {
+          attributes: {
+            eventType: string;
+            firstName: string;
+            lastName: string;
+            isAnonymous: string;
+            email: string;
+            phoneNumber: string;
+          };
+        };
+      }) => void;
+      Personalization: {
+        fetch: (
+          category: [string],
+          options?: { anchorId?: string }
+        ) => Promise<{ personalizations: { attributes: { imageURL: string }; data: ProductType[] }[] }>;
+      };
     };
     embeddedservice_bootstrap: {
       settings: {
