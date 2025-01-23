6c44110cf90407ce779438b40fbef69b484b29a2
mvrzan <mvrzan@icloud.com>
2025-01-23 09:23:32 -0500

    refactor App component to integrate loadAgentforceScript hook for loading Agentforce script

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import { useEffect } from "react";
 import useBearStore from "./hooks/useBearStore";
-import useAgentforceScript from "./hooks/useAgentforceScript";
 import useLoadDataCloudScript from "./hooks/useLoadDataCloudScript";
+import useLoadAgentforceScript from "./hooks/useLoadAgenforceScript";
 
 import { Theme } from "@chakra-ui/react";
-import { readFromLocalStorage } from "./utils/localStorageUtil";
-
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Body from "./components/Body/Body";
@@ -14,37 +12,16 @@ import ChatWidget from "./components/ChatWidget/ChatWidget";
 import AgentChatWidget from "./components/ChatWidget/AgentChatWidget";
 
 const App = () => {
-  const configureAgentforceScriptUrl = useAgentforceScript();
   const loadDataCloudScript = useLoadDataCloudScript();
+  const loadAgentforceScript = useLoadAgentforceScript();
 
   const theme = useBearStore((state) => (state.theme === "dark" || state.theme === "light" ? state.theme : "light"));
   const chatSelector = useBearStore((state) => state.chatSelector);
-  const setChatSelector = useBearStore((state) => state.setChatSelector);
 
   useEffect(() => {
     loadDataCloudScript();
-
-    const existingAgentforceScript = document.querySelector('script[src*="assets/js/bootstrap.min.js"]');
-
-    if (!existingAgentforceScript) {
-      const agentforceConfig = readFromLocalStorage("agentforceConfig");
-
-      if (agentforceConfig) {
-        const agentforceScriptUrl = JSON.parse(agentforceConfig);
-        configureAgentforceScriptUrl(
-          agentforceScriptUrl?.orgId,
-          agentforceScriptUrl?.scriptUrl,
-          agentforceScriptUrl?.instanceUrl,
-          agentforceScriptUrl?.embeddingUrl,
-          agentforceScriptUrl?.embeddingApiName
-        );
-
-        setChatSelector(true);
-      }
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    loadAgentforceScript();
+  }, [loadDataCloudScript, loadAgentforceScript]);
 
   return (
     <Theme appearance={theme}>
